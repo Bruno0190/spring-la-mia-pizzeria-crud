8bad566b8b29ef6e43660ed0f06d9749ed74bcda
Bruno0190 <bruno.varadero90@gmail.com>
2025-07-22 17:29:17 +0200

    feat: commenti

--- src/main/java/org/java/spring/pizzeria/repository/PizzaRepository.java
+++ src/main/java/org/java/spring/pizzeria/repository/PizzaRepository.java
@@ -4,7 +4,12 @@ import org.java.spring.pizzeria.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-/*Mentre extends nella classi si usa per ereditare da una superclasse, nelle interfacce, extends eredita da un altra interfaccia. JpaRepository è un interfaccia già integrata in Spring */
+/*Mentre extends nella classi si usa per ereditare da una superclasse, nelle interfacce, extends eredita da un altra interfaccia. JpaRepository è un interfaccia già integrata in Spring.
+In sostanza i file marcati come Repository sono delle interfacce Java perchè l'interfaccia è in effetti un file java che definisce quali metodi devono esserci senza scriverli. Infatti estendendo JpaRepository<Classe, attributo di riconoscimento> quest'interfaccia va ad implementare tutti i metodi
+- .findAll();
+- .findById(ID);
+- .save(classeObj);
+- .deleteById(ID); */
 public interface PizzaRepository extends JpaRepository<Pizza, Long> {
 
 }
